5bfcf00c5a5a661878392543b435a90c50a49963
Daniel Balchev <dbalchev@gmail.com>
2023-12-01 08:31:08 +0000

    Solve Day01

--- app/Day01.hs
+++ app/Day01.hs
@@ -2,46 +2,79 @@
 
 module Day01 where
 
-import           Data.Char                    (isSpace)
-import qualified Data.Text.Lazy               as LT
-import           Data.Text.Lazy.Read          (decimal, signed)
-import qualified Data.Vector                  as DV
-import           Data.Vector.Algorithms.Merge (sort)
-import           Text.Parsec                  (many1, newline, optional, parse,
-                                               satisfy)
-import           Text.Parsec.Text.Lazy        (Parser, parseFromFile)
+import qualified Control.Exception     as CE
+import qualified Data.List
+import           Data.Char             (isDigit)
+import           Data.Maybe            (isJust)
+import qualified Data.Text.Lazy        as LT
+import qualified Data.Text.Lazy.IO     as LTIO
+import qualified Data.Vector           as DV
+import Control.Monad (guard)
 
-myRead word = x
+parseLine1 :: LT.Text -> Int
+parseLine1 line = read [firstDigit, lastDigit]
     where
-        Right (x, _) = signed decimal word
+        digits = filter isDigit $ LT.unpack line
+        firstDigit : _ = digits
+        lastDigit = last digits
 
-withOptionalNewLine :: Parser a -> Parser a
-withOptionalNewLine parser = do
-    parsed <- parser
-    optional newline
-    return parsed
+-- >>> parseLine1 $ LT.pack "a1b2c3d4e5f"
+-- 15
 
-
-inputParser = DV.fromList <$> many1 anElf
+parseToken2 :: LT.Text -> Maybe Int
+parseToken2 suffix = fst <$> Data.List.uncons matches
     where
-        singleLine = myRead . LT.pack <$> withOptionalNewLine (many1 $ satisfy (not . isSpace))
-        anElf = withOptionalNewLine $ DV.fromList <$> many1 singleLine
--- >>> parse inputParser "" "1234\n4567\n\n12\n34"
--- Right [[1234,4567],[12,34]]
+        options = [(["0"], 0),
+                   (["1", "one"], 1),
+                   (["2", "two"], 2),
+                   (["3", "three"], 3),
+                   (["4", "four"], 4),
+                   (["5", "five"], 5),
+                   (["6", "six"], 6),
+                   (["7", "seven"], 7),
+                   (["8", "eight"], 8),
+                   (["9", "nine"], 9)]
+        matches :: [Int]
+        matches = do
+            (variants, result) <- options
+            guard $ any (`LT.isPrefixOf` suffix) variants
+            return result
 
-solve :: LT.Text -> IO (Int, Int)
+
+-- >>> parseToken2 "three"
+-- Just 3
+
+suffixes :: LT.Text -> [LT.Text]
+suffixes text = maybe [] (\(_, suffix) -> text: suffixes suffix) $ LT.uncons text
+-- >>> suffixes $ LT.pack "foobar"
+-- ["foobar","oobar","obar","bar","ar","r"]
+
+
+parseLine2 :: LT.Text -> Int
+parseLine2 line = 10 * firstDigit + lastDigit
+    where
+        maybeDigits = map parseToken2 $ suffixes line
+        digits = filter isJust maybeDigits
+        Just firstDigit = head digits
+        Just lastDigit = last digits
+
+-- >>> parseLine2 $ LT.pack "eight5oneights"
+-- 88
+
+solve :: String -> IO (Either CE.SomeException Int, Either CE.SomeException Int)
 solve inputFilename = do
-    Right parsedInput <- parseFromFile inputParser $ LT.unpack inputFilename
-    let sums = DV.map DV.sum parsedInput
-    let solution1 = DV.maximum sums
-    print solution1
-    let sortedSums = DV.modify sort sums
-    let solution2 = DV.sum $ DV.slice (DV.length sortedSums - 3) 3 sortedSums
-    print solution2
-    return (solution1, solution2)
+    inputText <- LTIO.readFile inputFilename
+    let lines = DV.fromList $ filter (not . LT.null) $ LT.lines inputText
+    solution1 <- CE.try . CE.evaluate . DV.sum . DV.map parseLine1 $ lines
+    solution2 <- CE.try . CE.evaluate . DV.sum . DV.map parseLine2 $ lines
 
--- >>> solve "inputs/sample/01.txt"
--- (24000,45000)
+    return (solution1, solution2)
+-- >>> solve "inputs/sample/01.1.txt"
+-- (Right 142,Right 142)
+
+-- >>> solve "inputs/sample/01.2.txt"
+-- (Left /workspaces/advent-2023/app/Day01.hs:18:9-31: Non-exhaustive patterns in firstDigit : _
+-- ,Right 281)
 
 -- >>> solve "inputs/real/01.txt"
--- (67658,200158)
+-- (Right 55971,Right 54719)
--- tests/Day01_test.hs
+++ tests/Day01_test.hs
@@ -1,15 +1,6 @@
-{-# LANGUAGE OverloadedLists   #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Day01_test where
-import qualified Data.Vector as DV
-import           Day01       (inputParser)
 import           Test.HUnit  (Test (TestCase, TestLabel, TestList), assertEqual)
-import           Text.Parsec (parse)
 
-parseTest = TestCase $ do
-    let actual = parse inputParser "" "1234\n4567\n\n12\n34"
-    let expected = Right [[1234, 4567],[12, 34]]
-    assertEqual "parsed result is different" actual expected
-
-day01Suite = TestList [TestLabel "parseTest" parseTest]
+day01Suite = TestList []
