532d5593cd8dfb8bb169f279d9f68ceca727d7be
Daniel Balchev <dbalchev@gmail.com>
2023-12-04 07:16:29 +0000

    Day 04 part 2

--- app/Day04.hs
+++ app/Day04.hs
@@ -1,13 +1,14 @@
 module Day04 where
+import           Control.Monad        (forM_)
 import qualified Data.Text            as T
 import qualified Data.Text.IO         as T
 import           Data.Vector          (Vector)
 import qualified Data.Vector          as DV
+import qualified Data.Vector.Mutable  as DVM
 import           Text.Parsec          (parse, sepBy, spaces, string)
 import qualified Text.Parsec.Language as TPL
 import           Text.Parsec.String   (Parser)
 import qualified Text.Parsec.Token    as TPT
-
 
 data Card = Card { cardIndex :: Int, winningNumbers :: Vector Int, myNumbers :: Vector Int}
     deriving Show
@@ -30,17 +31,27 @@ lineParser = do
 -- >>> parse lineParser "" "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"
 -- Right (Card {cardIndex = 2, winningNumbers = [13,32,20,16,61], myNumbers = [61,30,68,82,17,32,24,19]})
 
-score (Card _ winningNumbers myNumbers) = if matches < 1 then 0 else 2 ^ (matches - 1)
-    where 
-        matches = length . filter (`DV.elem` winningNumbers) . DV.toList $ myNumbers 
+countMatches (Card _ winningNumbers myNumbers)
+    = length . filter (`DV.elem` winningNumbers) . DV.toList $ myNumbers
+
+score card = if matches < 1 then 0 else 2 ^ (matches - 1)
+    where
+        matches = countMatches card
 
 solve inputFilename = do
     Right cards <-  fmap DV.fromList . traverse (parse lineParser "") . filter (not . null) . map T.unpack . T.lines <$> T.readFile inputFilename
     let solution1 = sum $ map score . DV.toList $ cards
-    return solution1
+    repetitions <- DVM.replicate (DV.length cards) (1 :: Int)
+    forM_ (zip [0..] (DV.toList cards)) $ \(i, card) -> do
+        let matches = countMatches card
+        cardRepetitions <- DVM.read repetitions i
+        forM_ [i + 1..i + matches] (DVM.modify repetitions (+ cardRepetitions))
+    endRepetitions <- DV.freeze repetitions
+    let solution2 = DV.sum endRepetitions
+    return (solution1, solution2)
 
 -- >>> solve "inputs/sample/04.txt"
--- 13
+-- (13,30)
 
 -- >>> solve "inputs/real/04.txt"
--- 24848
+-- (24848,7258152)
