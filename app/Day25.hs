ff8bfb991601199b670fd7e5058f3257ec7d7b0a
Daniel Balchev <daniel.balchev@microblink.com>
2024-07-12 15:21:53 +0000

    Simplify compileGraph

--- app/Day25.hs
+++ app/Day25.hs
@@ -6,6 +6,7 @@ import qualified Data.Text               as T
 import qualified Data.Text.IO            as T
 import qualified Data.Vector             as V
 import qualified Data.Vector.Mutable     as MV
+import qualified Data.HashMap            as HM
 
 import           Control.Monad           (filterM, forM, forM_, guard, unless,
                                           when)
@@ -24,41 +25,37 @@ parseLine line = (node1, nodes)
         nodes = T.words nodes_concat
 
 
-newtype Vertex = Vertex (Int, T.Text, V.Vector (Int, Vertex))
+newtype Vertex = Vertex (Int, T.Text, V.Vector (Int, Vertex, Int))
 
-compileGraph :: [(T.Text, [T.Text])] -> V.Vector Vertex
-compileGraph parsedGraph = runST $ mdo
-    vertexNameToIndex <- HT.new
-    forM parsedGraph $  \(vertexName, _) -> do
-        currentSize <- HT.size vertexNameToIndex
-        HT.insert vertexNameToIndex vertexName currentSize
-    n <- HT.size vertexNameToIndex
-    vertexIndexToName <- MV.replicate n ""
-    (`HT.mapM_` vertexNameToIndex) $ \(name, index) -> do
-        MV.write vertexIndexToName index name
-    edgesByVertex <- forM parsedGraph $ \(vertex1Name, vertex2Names) -> do
-        Just vertex1Index <- HT.lookup vertexNameToIndex vertex1Name
-        Just vertex2Indices <- sequence <$> traverse (HT.lookup vertexNameToIndex) vertex2Names
-        return $ do
-            vertex2Index <- vertex2Indices
-            [(vertex1Index, vertex2Index), (vertex2Index, vertex1Index)]
-    let edges = ((`zip` [0..]) . sort . concat) edgesByVertex
-    let groupedEdges = groupBy  (\((lh, _), _) ((rh, _), _) -> lh == rh) edges
-    compiledGraph <- V.fromList <$> forM groupedEdges (\edgeGroup -> do
-        let ((selfIndex, _), _): _ = edgeGroup
-        selfName <- MV.read vertexIndexToName selfIndex
-        let adjVertices = V.fromList $ map (\((_, adjIndex), edgeIndex) -> (edgeIndex, compiledGraph V.! adjIndex)) edgeGroup
-        return $ Vertex (selfIndex, selfName, adjVertices))
-    return compiledGraph
+compileGraph parsedGraph = V.fromList (HM.elems nameToVertex)
+    where
+        edgePairs = do
+            (first, seconds) <- parsedGraph
+            second <- seconds
+            return (first, second)
+        edgeIndexedPairs = do
+            (i, (first, second)) <- zip [0..] edgePairs
+            let directIndex = 2 * i
+            let inverseIndex = 2 * i + 1
+            [((first, second), (directIndex, inverseIndex)), ((second, first), (inverseIndex, directIndex))]
+        isSameFirstVertex ((a, _), _) ((b, _), _) = a == b
+        groupedEdges = groupBy isSameFirstVertex . sort $ edgeIndexedPairs
+        makeEdge ((_, second), (directIndex, inverseIndex)) = (directIndex, nameToVertex HM.! second, inverseIndex)
+        makeVertex name index edges = Vertex (index, name, V.fromList $ map makeEdge edges)
+        nameToVertex = HM.fromList $ do
+            (index, edgeGroup) <- zip [0..] groupedEdges
+            let ((first, _), (_, _)) : _ = edgeGroup
+            return (first, makeVertex first index edgeGroup)
+
 
 testCompileGraph = unvertex <$> cg
     where
         cg = compileGraph [("foo", ["bar", "baz"]), ("bar", ["baz"]), ("baz", ["foobar"]), ("foobar", [])]
-        getName (index, Vertex (_, name, _)) = (index, name)
+        getName (index, Vertex (_, name, _), _) = (index, name)
         unvertex (Vertex (index, name, adj)) = (index, name, getName  <$> adj)
 
 -- >>> testCompileGraph
--- [(0,"foo",[(0,"bar"),(1,"baz")]),(1,"bar",[(2,"foo"),(3,"baz")]),(2,"baz",[(4,"foo"),(5,"bar"),(6,"foobar")]),(3,"foobar",[(7,"baz")])]
+-- [(0,"bar",[(4,"baz"),(1,"foo")]),(1,"baz",[(5,"bar"),(3,"foo"),(6,"foobar")]),(2,"foo",[(0,"bar"),(2,"baz")]),(3,"foobar",[(7,"baz")])]
 
 
 
